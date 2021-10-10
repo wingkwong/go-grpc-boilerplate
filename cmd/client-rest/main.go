c207c99fa389c35e05b6a24b0d8db0a36d7ba5cb
Wing-Kam Wong <wingkwong.code@gmail.com>
2021-10-11 01:02:53 +0800

    init: client-rest main.go

---
+++ cmd/client-rest/main.go
@@ -0,0 +1,108 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strings"
+	"time"
+)
+
+func main() {
+	address := flag.String("server", "http://localhost:8080", "HTTP gateway url, e.g. http://localhost:8080")
+	flag.Parse()
+
+	t := time.Now().In(time.UTC)
+	pfx := t.Format(time.RFC3339Nano)
+
+	// Create
+	resp, err := http.Post(*address+"/v1/foo", "application/json", strings.NewReader(fmt.Sprintf(`
+		{
+			"api":"v1",
+			"foo": {
+				"title":"title (%s)",
+				"desc": "desc (%s)"
+			}
+		}
+	`, pfx, pfx)))
+	if err != nil {
+		log.Fatalf("[ERROR] Failed to call Create method: %v", err)
+	}
+	bodyBytes, err := ioutil.ReadAll(resp.Body)
+	resp.Body.Close()
+
+	var body string
+	if err != nil {
+		body = fmt.Sprintf("[ERROR] Failed to read Create response body: %v", err)
+	} else {
+		body = string(bodyBytes)
+	}
+	log.Printf("[INFO] Create response: Code=%d, Body=%s\n\n", resp.StatusCode, body)
+
+	var created struct {
+		API string `json:"api"`
+		ID  string `json:"id"`
+	}
+
+	err = json.Unmarshal(bodyBytes, &created)
+	if err != nil {
+		log.Fatalf("[ERROR] Failed to unmarshal JSON response of Create method: %v", err)
+		fmt.Println("error:", err)
+	}
+
+	resp, err = http.Get(fmt.Sprintf("%s%s/%s", *address, "/v1/todo", created.ID))
+	if err != nil {
+		log.Fatalf("[ERROR] Failed to call Read method: %v", err)
+	}
+	bodyBytes, err = ioutil.ReadAll(resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		body = fmt.Sprintf("failed read Read response body: %v", err)
+	} else {
+		body = string(bodyBytes)
+	}
+	log.Printf("Read response: Code=%d, Body=%s\n\n", resp.StatusCode, body)
+
+	// Update
+	req, _ := http.NewRequest("PUT", fmt.Sprintf("%s%s/%s", *address, "/v1/foo", created.ID),
+		strings.NewReader(fmt.Sprintf(`
+		{
+			"api":"v1",
+			"foo": {
+				"title":"title (%s) + updated",
+				"desc":"desc (%s) + updated"
+			}
+		}
+	`, pfx, pfx)))
+	req.Header.Set("Content-Type", "application/json")
+	resp, err = http.DefaultClient.Do(req)
+	if err != nil {
+		log.Fatalf("[ERROR] Failed to call Update method: %v", err)
+	}
+	bodyBytes, err = ioutil.ReadAll(resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		body = fmt.Sprintf("[ERROR] Failed read Update response body: %v", err)
+	} else {
+		body = string(bodyBytes)
+	}
+	log.Printf("[INFO] Update response: Code=%d, Body=%s\n\n", resp.StatusCode, body)
+
+	// Delete
+	req, _ = http.NewRequest("DELETE", fmt.Sprintf("%s%s/%s", *address, "/v1/todo", created.ID), nil)
+	resp, err = http.DefaultClient.Do(req)
+	if err != nil {
+		log.Fatalf("[ERROR] Failed to call Delete method: %v", err)
+	}
+	bodyBytes, err = ioutil.ReadAll(resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		body = fmt.Sprintf("[ERROR] Failed read Delete response body: %v", err)
+	} else {
+		body = string(bodyBytes)
+	}
+	log.Printf("[INFO] Delete response: Code=%d, Body=%s\n\n", resp.StatusCode, body)
+}
