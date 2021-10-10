7355c215f7b8024721fa276c8d6590f176426c44
Wing-Kam Wong <wingkwong.code@gmail.com>
2021-10-11 01:27:39 +0800

    fix: wrong api path

--- pkg/api/v1/foo-service.pb.gw.go
+++ pkg/api/v1/foo-service.pb.gw.go
@@ -619,7 +619,7 @@ func RegisterFooServiceHandlerClient(ctx context.Context, mux *runtime.ServeMux,
 var (
 	pattern_FooService_Create_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"api", "v1", "foo"}, "", runtime.AssumeColonVerbOpt(true)))
 
-	pattern_FooService_Read_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 1, 0, 4, 1, 5, 2}, []string{"v1", "foo", "id"}, "", runtime.AssumeColonVerbOpt(true)))
+	pattern_FooService_Read_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 1, 0, 4, 1, 5, 3}, []string{"api", "v1", "foo", "id"}, "", runtime.AssumeColonVerbOpt(true)))
 
 	pattern_FooService_Update_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 1, 0, 4, 1, 5, 3}, []string{"api", "v1", "foo", "foo.id"}, "", runtime.AssumeColonVerbOpt(true)))
 
