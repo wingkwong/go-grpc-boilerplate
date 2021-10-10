8569b863b7053b70b79bffa070dac5bab4639c6e
Wing-Kam Wong <wingkwong.code@gmail.com>
2021-10-10 23:56:48 +0800

    init: rest server

---
+++ pkg/protocol/rest/server.go
@@ -0,0 +1,43 @@
+package rest
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/grpc-ecosystem/grpc-gateway/runtime"
+	"google.golang.org/grpc"
+
+	v1 "github.com/wingkwong/go-grpc-boilerplate/pkg/api/v1"
+)
+
+func RunServer(ctx context.Context, grpcPort, httpPort string) error {
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	mux := runtime.NewServeMux()
+	opts := []grpc.DialOption{grpc.WithInsecure()}
+	if err := v1.RegisterFooServiceHandlerFromEndpoint(ctx, mux, "localhost:"+grpcPort, opts); err != nil {
+		log.Fatalf("[ERROR] FZailed to start HTTP gateway: %v", err)
+	}
+
+	srv := &http.Server{
+		Addr:    ":" + httpPort,
+		Handler: mux,
+	}
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	go func() {
+		_, cancel := context.WithTimeout(ctx, 5*time.Second)
+		defer cancel()
+
+		_ = srv.Shutdown(ctx)
+	}()
+
+	log.Println("starting HTTP/REST gateway...")
+	return srv.ListenAndServe()
+}
