fd1a469989b64af88e6104cc80de880e7f7ba5ca
Wing-Kam Wong <wingkwong.code@gmail.com>
2021-11-13 16:04:17 +0800

    test: add Test_fooServiceServer_Read

--- pkg/service/v1/foo-service_test.go
+++ pkg/service/v1/foo-service_test.go
@@ -5,7 +5,9 @@ import (
 	"errors"
 	"reflect"
 	"testing"
+	"time"
 
+	"google.golang.org/protobuf/types/known/timestamppb"
 	"gopkg.in/DATA-DOG/go-sqlmock.v1"
 
 	v1 "github.com/wingkwong/go-grpc-boilerplate/pkg/api/v1"
@@ -144,7 +146,118 @@ func Test_fooServiceServer_Create(t *testing.T) {
 }
 
 func Test_fooServiceServer_Read(t *testing.T) {
-	// TO BE IMPLEMENTED
+	ctx := context.Background()
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("[Error] '%s' was not expected when opening a stub database connection", err)
+	}
+	defer db.Close()
+	s := NewFooServiceServer(db)
+
+	curTime := time.Now()
+
+	type args struct {
+		ctx context.Context
+		req *v1.ReadRequest
+	}
+	tests := []struct {
+		name    string
+		s       v1.FooServiceServer
+		args    args
+		mock    func()
+		want    *v1.ReadResponse
+		wantErr bool
+	}{
+		{
+			name: "01 - OK",
+			s:    s,
+			args: args{
+				ctx: ctx,
+				req: &v1.ReadRequest{
+					ApiVersion: "v1",
+					Id:         1,
+				},
+			},
+			mock: func() {
+				rows := sqlmock.NewRows([]string{"ID", "Title", "Desc", "CreatedBy", "UpdatedBy", "CreatedAt", "UpdatedAt"}).
+					AddRow(1, "title", "description", "foo", "foo", curTime, curTime)
+				mock.ExpectQuery("SELECT (.+) FROM Foo").WithArgs(1).WillReturnRows(rows)
+			},
+			want: &v1.ReadResponse{
+				ApiVersion: "v1",
+				Foo: &v1.Foo{
+					Id:    1,
+					Title: "title",
+					Desc:  "description",
+					SysFields: &v1.SystemFields{
+						CreatedBy: "foo",
+						UpdatedBy: "foo",
+						CreatedAt: timestamppb.New(curTime),
+						UpdatedAt: timestamppb.New(curTime),
+					},
+				},
+			},
+		},
+		{
+			name: "02 - Unsupported API",
+			s:    s,
+			args: args{
+				ctx: ctx,
+				req: &v1.ReadRequest{
+					ApiVersion: "v1000",
+					Id:         1,
+				},
+			},
+			mock:    func() {},
+			wantErr: true,
+		},
+		{
+			name: "03 - SELECT failed",
+			s:    s,
+			args: args{
+				ctx: ctx,
+				req: &v1.ReadRequest{
+					ApiVersion: "v1",
+					Id:         1,
+				},
+			},
+			mock: func() {
+				mock.ExpectQuery("SELECT (.+) FROM foo").WithArgs(1).
+					WillReturnError(errors.New("SELECT Failed"))
+			},
+			wantErr: true,
+		},
+		{
+			name: "04 - Not found",
+			s:    s,
+			args: args{
+				ctx: ctx,
+				req: &v1.ReadRequest{
+					ApiVersion: "v1",
+					Id:         1,
+				},
+			},
+			mock: func() {
+				rows := sqlmock.NewRows([]string{"ID", "Title", "Desc", "CreatedBy", "UpdatedBy", "CreatedAt", "UpdatedAt"})
+				mock.ExpectQuery("SELECT (.+) FROM Foo").WithArgs(1).WillReturnRows(rows)
+			},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tt.mock()
+			got, err := tt.s.Read(tt.args.ctx, tt.args.req)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("fooServiceServer.Read() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+
+			if err == nil && !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("fooServiceServer.Read() = %v, want %v", got, tt.want)
+			}
+		})
+	}
 }
 
 func Test_fooServiceServer_Update(t *testing.T) {
